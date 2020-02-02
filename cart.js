75be01b66741ada87ec6e098ae7d09ccee876905
matekuna92 <kunamate66@gmail.com>
2020-02-02 15:27:46 +0100

    buttons added, can increase quantity

--- cart.js
+++ cart.js
@@ -10,7 +10,8 @@ addToCartButtons.forEach(button => {
         const product = {
             image: currentProduct.querySelector('.product__image').getAttribute('src'),
             name: currentProduct.querySelector('.product__name').innerText,
-            price: currentProduct.querySelector('.product__price').innerText
+            price: currentProduct.querySelector('.product__price').innerText,
+            quantity: 1
         };
 
         // ellenőrizni kell, hozzá lett-e már adva a product korábban
@@ -21,15 +22,41 @@ addToCartButtons.forEach(button => {
         {
             // ES6 template string, így nincs szükség string összefűzésre
             cartDom.insertAdjacentHTML('beforeend',
-            `<div class="cart-item">
+            `<div class="cart__item">
                 <img class="cart__item__image" src="${product.image}" alt="${product.name}">
-                <h2 class="cart__product__name"> ${product.name} </h2>
-                <h3 class="cart__product__price"> ${product.price} </h3>
+                <h2 class="cart__item__name"> ${product.name} </h2>
+                <h3 class="cart__item__price"> ${product.price} </h3>
+                <h3 class="cart__item__quantity"> ${product.quantity} </h3>
+                <button class="btn btn--primary btn--small" data-action="DECREASE_QUANTITY"> &minus; </button>
+                <button class="btn btn--primary btn--small" data-action="INCREASE_QUANTITY"> &plus; </button>
+                <button class="btn btn--danger btn--small" data-action="DELETE_PRODUCT"> &times; </button>
+                
             </div>`
             );
 
             cart.push(product);
             button.innerText = 'Already in cart';
+
+            let cartItems = cartDom.querySelectorAll('.cart__item');
+            
+            cartItems.forEach(cartItem => {
+                if( cartItem.querySelector('.cart__item__name').innerText === product.name )
+                {
+                    cartItem.querySelector('[data-action="INCREASE_QUANTITY"]')
+                    .addEventListener('click', () => {
+                        cart.forEach(item => {
+                            if(item.name === product.name)
+                            {
+                                //item.quantity = item.quantity++;
+                                cartItem.querySelector('.cart__item__quantity').innerText = ++item.quantity;
+                    // item.quantity++ esetén első kattintásra nem növeszik az érték mert ekkor
+                    // először megjelenik az érték, aztán növeli az értékét.
+                    // ++item.quantity esetén először növeli meg az értéket, aztán jelenik meg
+                            }
+                        })
+                    })
+                }
+            });
         }
     });
 });
