7519942db97264181407be4bc44538d9c46a6d3c
matekuna92 <kunamate66@gmail.com>
2020-02-06 22:56:17 +0100

    add dom for each storage item, buttons working, color bug

--- cart.js
+++ cart.js
@@ -10,6 +10,8 @@ let cart = JSON.parse(localStorage.getItem('cart')) || [];
 // minden elemre, ami a storage tömbben van jöjjön létre a div
 
 // ha vannak itemek a tömbben, hozzáadjuk a dom-hoz ugyanúgy, mintha az Add to cart gombot használnánk
+// ekkor a click események nem működnek  a gombokon, mert az if-en kívül vannak eredetileg
+
 if( cart.length > 0 )
 {
     cart.forEach( (product) => {
@@ -25,7 +27,107 @@ if( cart.length > 0 )
                 
             </div>`
             );
-    })
+
+            // localstorage esetén is kezelni kell, ha már benne van a tömbben az item, legyen disabled a gomb
+            // nem kell külön eventListenert létrehozni 
+            addToCartButtons.forEach( button => {
+                let currentProduct = button.parentNode;
+
+                // ha a szülő elem neve megegyezik a localstorage-ban tárolt product nevével, akkor disabled
+                if(currentProduct.querySelector('.product__name').innerText === product.name)
+                {
+                    button.innerText = 'Already in cart';
+                    button.disabled = true;
+
+                    // a gombok működését is átmásoljuk a lenti kódból
+
+                    let cartItems = cartDom.querySelectorAll('.cart__item');
+            
+                cartItems.forEach(cartItem => {
+                    if( cartItem.querySelector('.cart__item__name').innerText === product.name )
+                    {
+                        cartItem.querySelector('[data-action="INCREASE_QUANTITY"]')
+                        .addEventListener('click', () => {
+                            cart.forEach(item => {
+                                if(item.name === product.name)
+                                {
+                                    cartItem.querySelector('.cart__item__quantity').innerText = ++item.quantity;
+                                    // item.quantity++ esetén első kattintásra nem növeszik az érték mert ekkor
+                                    // először megjelenik az érték, aztán növeli az értékét.
+                                    // ++item.quantity esetén először növeli meg az értéket, aztán jelenik meg
+                                    cartItem.querySelector('[data-action="DECREASE_QUANTITY"]').classList.remove('btn--danger');
+                                    // minden egyes változást is menteni kell a storage-ba
+                                    localStorage.setItem('cart', JSON.stringify(cart));
+                                }
+                            })
+                        })
+
+                        cartItem.querySelector('[data-action="DECREASE_QUANTITY"]')
+                        .addEventListener('click', () => {
+                            cart.forEach(item => {
+                                if(item.name === product.name)
+                                {
+                                    if( item.quantity > 1 )
+                                    {
+                                        cartItem.querySelector('.cart__item__quantity').innerText = --item.quantity;
+                                        localStorage.setItem('cart', JSON.stringify(cart));
+                                    }
+                                    else
+                                    {
+                                        cartItem.classList.add('cart__item__removed');
+                                        setTimeout( () => cartItem.remove(), 250);
+                                        
+                                        // magában a cart tömmben viszont még mindig megmarad a termék
+                                        console.log(cart);
+                                        cart = cart.filter( cartItem => cartItem.name !== product.name);
+                                        localStorage.setItem('cart', JSON.stringify(cart));
+                                        console.log(cart);
+
+                                        // gomb visszaállítása disabled állapotról
+                                        button.innerText = 'Add to cart';
+                                        button.disabled = false;
+                                    }
+
+                                    if( item.quantity == 1 )
+                                    {
+                                        cartItem.querySelector('[data-action="DECREASE_QUANTITY"]').classList.add('btn--danger');
+                                    }
+                                }
+                            })
+                        })
+
+                        cartItem.querySelector('[data-action="DELETE_PRODUCT"]')
+                        .addEventListener('click', () => {
+                            cart.forEach(item => {
+                                if(item.name === product.name)
+                                {
+                                    if( item.quantity > 1 )
+                                    {
+                                        cartItem.querySelector('.cart__item__quantity').innerText = --item.quantity;
+                                    }
+                                    else
+                                    {
+                                        cartItem.classList.add('cart__item__removed');
+                                        setTimeout( () => cartItem.remove(), 250);
+                                        
+                                        // magában a cart tömmben viszont még mindig megmarad a termék
+                                        console.log(cart);
+                                        cart = cart.filter( cartItem => cartItem.name !== product.name);
+                                        localStorage.setItem('cart', JSON.stringify(cart));
+                                        console.log(cart);
+
+                                        // gomb visszaállítása disabled állapotról
+                                        button.innerText = 'Add to cart';
+                                        button.disabled = false;
+                                    }
+                                }
+                            })
+                        })
+                    }
+                });
+                }
+            }) 
+        })
 }
 
 // ES6
